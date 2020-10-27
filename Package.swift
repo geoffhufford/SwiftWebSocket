93f93789b6aad36d50c9c7f80199307e0a8ae573
Piotr Ożóg <piotr.ozog@zen.com>
2020-10-27 14:40:03 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,5 +2,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftWebSocket"
+    name: "SwiftWebSocket",
+    platforms: [.iOS(.v9)],
+    // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
+    products: [
+        .library(name: "SwiftWebSocket", targets: ["SwiftWebSocket"])
+    ]
 )
